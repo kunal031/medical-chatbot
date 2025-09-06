93d275e570c59eab2d110e5bf9ff75112e5a53a8
Kunal Raj <kunal@Kunals-MacBook-Air.local>
2025-09-06 19:20:06 +0530

    folder structure added

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import find_packages, setup
+
+setup (
+
+  name = "Generative AI Project",
+  version='0.0.0',
+  author= 'Kunal Kashyap',
+  author_email= 'kunalraj0531@gmail.com',
+  packages=  find_packages(),
+  install_requires = []
+
+)
